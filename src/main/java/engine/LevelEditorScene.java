901d08827d643c1a038e422cccf324be05177ede
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2021-05-14 21:49:06 +0300

    my-first-draw

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -1,7 +1,13 @@
 package engine;
 
 
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
 import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.*;
 
 public class LevelEditorScene extends  Scene{
 
@@ -26,14 +32,33 @@ public class LevelEditorScene extends  Scene{
 
     private int vertexID, fragmentID, shaderProgram;
 
+    private float[] vertexArray = {
+        //position              //color
+            0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, 1.0f, // bottom right
+            -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f, 1.0f, //top left
+            0.5f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f, // top right
+            -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f, 1.0f  // bottom left
+    };
+    //Important: Must be in counter-clockwise order
+    private int[] elementArray = {
+
+
+            2, 1, 0, // top right triangle
+            0, 1, 3 // bottom left triangle
+    };
+
+    private int vaoID, vboID, eboID;
+
     public LevelEditorScene(){
 
     }
 
     @Override
     public void init()
-    {
+    {   //=========================
         // compile and link shader
+        //=========================
+
 
         // First load and compile vertex shader
         vertexID = glCreateShader(GL_VERTEX_SHADER);
@@ -85,11 +110,64 @@ public class LevelEditorScene extends  Scene{
             assert  false: "";
         }
 
+        //==========================================================
+        //Generate VAO, VBO and EBO buffer objects, and send to GPU
+        //===========================================================
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        //create float buffer of vertex
+
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        //Create VBO upload the vertex buffer
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        //Create the indices and upload
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        //add the vertex attribute pointers
+        int positionsSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeByte = (positionsSize + colorSize) * floatSizeBytes;
+
+        glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeByte, 0);
+        glEnableVertexAttribArray(0); // this index - is index layout location in shader
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeByte, positionsSize * floatSizeBytes);
+        glEnableVertexAttribArray(1);
+
     }
 
     @Override
     public void update(float dt) {
+        // Bind shader program
+        glUseProgram(shaderProgram);
+        //bind the VAo that using
+        glBindVertexArray(vaoID);
+
+        //enable the vertex attribute pointers
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, elementArray.length,GL_UNSIGNED_INT, 0);
 
 
+        //Unbind everything;
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+
+        glUseProgram(0);
     }
 }
