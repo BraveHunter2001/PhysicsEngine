af7cd7b8568944fa10a6b51099185b4573b1a918
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2021-05-14 21:52:14 +0300

    my-first-draw

--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -36,7 +36,7 @@ public class LevelEditorScene extends  Scene{
         //position              //color
             0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f, 1.0f, // bottom right
             -0.5f, 0.5f, 0.0f,    0.0f, 1.0f, 0.0f, 1.0f, //top left
-            0.5f, 0.5f, 0.0f,     0.0f, 0.0f, 1.0f, 1.0f, // top right
+            0.5f, 0.5f, 0.0f,     1.0f, 0.0f, 1.0f, 1.0f, // top right
             -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 0.0f, 1.0f  // bottom left
     };
     //Important: Must be in counter-clockwise order
