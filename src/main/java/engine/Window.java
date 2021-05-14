e0d6cf76c6b4a1e52fc540a14663ad12ef0857da
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2021-05-14 19:38:42 +0300

    add-class-Time

--- src/main/java/engine/Window.java
+++ src/main/java/engine/Window.java
@@ -3,6 +3,7 @@ package engine;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
+import until.Time;
 
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
@@ -15,25 +16,28 @@ public class Window {
     private long glfwWindow;
 
     private static Window window = null;
+    private float r, g, b, a;
+    private boolean fadeToBack = false;
 
 
-    private Window()
-    {
+    private Window() {
         this.width = 900;
         this.height = 600;
         this.title = "PhysicsEngine";
+        r = 1;
+        b = 1;
+        g = 1;
+        a = 1;
     }
-    public static Window get()
-    {
-        if(Window.window == null)
-        {
+
+    public static Window get() {
+        if (Window.window == null) {
             Window.window = new Window();
         }
         return Window.window;
     }
 
-    public void run()
-    {
+    public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
         init();
@@ -48,13 +52,12 @@ public class Window {
         glfwSetErrorCallback(null).free();
     }
 
-    public void init()
-    {
+    public void init() {
         //Setup an error callBack
         GLFWErrorCallback.createPrint(System.err).set();
 
         //init glfw
-        if(!glfwInit()){
+        if (!glfwInit()) {
             throw new IllegalStateException("Unable to initialize GLFW");
         }
 
@@ -65,15 +68,15 @@ public class Window {
         //glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE); // start max
 
         //create window
-        glfwWindow = glfwCreateWindow(this.width,this.height,this.title, NULL, NULL);
-        if (glfwWindow == NULL)
-        {
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
             throw new IllegalStateException("Failed to create GLFW window");
         }
         // add callback for MouseListener
         glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
         glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
         glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
 
         //Make the OpenGL context current
         glfwMakeContextCurrent(glfwWindow);
@@ -88,12 +91,33 @@ public class Window {
     }
 
     public void loop() {
-        while (!glfwWindowShouldClose(glfwWindow)){
+        float beginTime = Time.getTime();
+        float endTime;
+
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // poll events
             glfwPollEvents();
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
+            if (fadeToBack) {
+                r = Math.max(r - 0.01f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.01f, 0);
+            }
+
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
+                fadeToBack = true;
+            }
+
             glfwSwapBuffers(glfwWindow);
+
+            // time calc
+            endTime = Time.getTime();
+            float dt = endTime - beginTime;
+            beginTime = endTime;
         }
     }
 }
